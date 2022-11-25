1fde08b2b095f5c8fcebf52cf596d5e46482aad2
Kiran <Kiran@DESKTOP-02956BS>
2022-11-25 14:20:08 +0530

    Name Added to ReadCommandLine Test Script

--- src/test/java/VtigerPractice/ReadDataFromCmdLine.java
+++ src/test/java/VtigerPractice/ReadDataFromCmdLine.java
@@ -11,11 +11,13 @@ public class ReadDataFromCmdLine {
 		String URL = System.getProperty("url");
 		String USERNAME = System.getProperty("username");
 		String PASSWORD = System.getProperty("password");
+		String NAME = System.getProperty("name");
 		
 		System.out.println(BROWSER);
 		System.out.println(URL);
 		System.out.println(USERNAME);
 		System.out.println(PASSWORD);
+		System.out.println(NAME);
 		
 		
 	}
